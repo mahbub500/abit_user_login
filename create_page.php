27d3be44e175da7980dc57958c39696445b46fcd
mahbubmr500 <mahbubmr500@gmail.com>
2023-05-27 01:36:49 +0600

    Now Page Data Insert

--- create_page.php
+++ create_page.php
@@ -1,32 +1,51 @@
 <?php 
 	// include_once( 'Session.php' );
 	include( 'inc/Header.php' );
+	include ( 'lib/page_create.php' );
+
+	$page = new Page();
+
+	if ( isset( $_POST['page_create'] )) {
+
+		// echo"<pre>";
+		// print_r( $_FILES['thumbnail']['name'] );
+		// echo"</pre>";
+
+		$pageCreate = $page->addPageData( $_POST  );
+
+		if ( isset( $pageCreate )) {
+			 echo"<pre>";
+				print_r( $pageCreate );
+				echo"</pre>";
+		}
+	}
+
 ?>
     <div class="row">
       <div class="col-md-6">
       	<h1>Create Page</h1>
-        <form action="" method="post">
+        <form action="" method="post" enctype="multipart/form-data">
 						<div class="form-group">
 							<label for="InputName">Name :</label>
-							<input type="text" class="form-control" name="page_name" id="InputName" placeholder="Page Name">
+							<input type="text" class="form-control" name="name" id="InputName" placeholder="Page Name">
 						</div>					
 						<div class="form-group">
 							<label for="InputMetaTitle">Meta Title :</label>
-							<input type="text" class="form-control" name="page_meta_title" id="InputMetaTitle" placeholder="Meta Title">
+							<input type="text" class="form-control" name="title" id="InputMetaTitle" placeholder="Meta Title">
 						</div>
 						<div class="form-group">
 							<label for="InputMetaDescription">Description :</label>
-							<input type="text" class="form-control" name="page_meta_title" id="InputMetaDescription" placeholder="Page Meta Description">
+							<input type="text" class="form-control" name="description" id="InputMetaDescription" placeholder="Page Meta Description">
 						</div>
 						<div class="form-group">
 							<label for="InputMetaContent">Content :</label>							
-							<textarea name="page_content" rows="5" cols="56" placeholder="Enter Your Content"></textarea>
+							<textarea name="content" rows="5" cols="56" placeholder="Enter Your Content"></textarea>
 						</div>
 						<div class="form-group">
 							<label for="PageThumbnail">Page Thumbnail :</label>
-							<input type="file" class="form-control-file" id="PageThumbnail">
+							<input type="file" name="thumbnail" class="form-control-file" id="PageThumbnail">
 						</div>
-						<button type="submit" class="btn btn-primary">Submit</button>
+						<button type="submit" name="page_create" class="btn btn-primary">Submit</button>
         </form>
       </div>  
       <div class="col-md-6"></div>  
--- lib/User.php
+++ lib/User.php
@@ -8,22 +8,6 @@ include( 'database.php' );
  */
 class User extends Database{
 
-	// private $servername = "localhost";
-	// private $username 	= "root";
-	// private $password 	= "337338";
-	// private $dbname 	= "abit.wp";
-	// public $con;
-
-	// public function __construct(){
-				
-	// 	$this->con = new mysqli( $this->servername, $this->username, $this->password, $this->dbname );
-	// 	if ( $this->con ) {
-	// 		echo "Seccess";
-	// 	}
-	// }
-
-	
-
 	// Add User
 	public function addUser( $data ){
 
